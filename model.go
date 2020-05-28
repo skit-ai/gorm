c98d9d0529630295327d3a1b4d61a31410e3977c
Aditya Satyavada <aditya@vernacular.ai>
2020-05-28 20:23:42 +0530

    add: New model which works for oracle at scale
    * If records go beyond 1000000, the ID returned by the oracle driver is of type float64 instead of uint(uint64)
    * To mitigate this issue, introducing the `primary` struct
    * The `primary` struct implements the `Scanner` interface to convert float64 and int64 into a unit ID
    * The `primary` struct implements the `Valuer` interface to return the uint value of the ID it encapsulates
    * Introducing the `ORM` struct which makes use of the `primary` struct to encapsulate the ID returned by the DB

--- model.go
+++ model.go
@@ -1,6 +1,9 @@
 package gorm
 
-import "time"
+import (
+	"database/sql/driver"
+	"time"
+)
 
 // Model base model definition, including fields `ID`, `CreatedAt`, `UpdatedAt`, `DeletedAt`, which could be embedded in your models
 //    type User struct {
@@ -8,6 +11,38 @@ import "time"
 //    }
 type Model struct {
 	ID        uint `gorm:"primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time `sql:"index"`
+}
+
+type primary struct {
+	ID uint
+}
+
+func (x *primary) Scan(src interface{}) error {
+	if val, ok := src.(uint); ok {
+		x.ID = val
+	} else if val, ok := src.(float64); ok {
+		// This happens due to oracle driver not knowing that uint is required by the calling code.
+		x.ID = uint(val)
+	} else if val, ok := src.(int64); ok {
+		x.ID = uint(val)
+	}
+	// Note: This else results in errors while inserting into the table in oracle. Hence not using such a clause
+	//else {
+	//	return errors.New(fmt.Sprintf("Unable to convert %v to uint", src))
+	//}
+
+	return nil
+}
+
+func (x *primary) Value() (driver.Value, error) {
+	return x.ID, nil
+}
+
+type ORM struct {
+	ID        primary `gorm:"primary_key"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt *time.Time `sql:"index"`
